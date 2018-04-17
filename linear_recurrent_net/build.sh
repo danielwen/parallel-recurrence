4a5f10fb54289ca9704d5befb1730c8f59f0b316
Daniel Wen <danielwen@users.noreply.github.com>
2018-04-17 23:28:08 +0000

    Fix build

--- linear_recurrent_net/build.sh
+++ linear_recurrent_net/build.sh
@@ -6,5 +6,6 @@ nvcc -c linear_recurrence.cu -o lib/linear_recurrence.o -O3 --compiler-options '
 nvcc lib/linear_recurrence.o -shared -o lib/liblinear_recurrence.so --compiler-options '-fPIC'
 
 # building tensorflow op
-export TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-g++ -std=c++11 -shared -o lib/tf_linear_recurrence.so linear_recurrent_net/tensorflow_binding/linear_recurrence_op.cpp lib/linear_recurrence.o -O3 -I $TF_INC -fPIC -lcudart
+export TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+g++ -std=c++11 -shared -o lib/tf_linear_recurrence.so tensorflow_binding/linear_recurrence_op.cpp lib/linear_recurrence.o -O3 -I $TF_INC -I $TF_INC/external/nsync/public -L $CUDA_HOME/lib64 -L $TF_LIB -fPIC -lcudart
